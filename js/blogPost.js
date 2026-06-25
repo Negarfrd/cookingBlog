5bcdad18d09266918baabc9fed55ecb779936634
Negarfrd <negar.fereidouni96@gmail.com>
2026-06-25 16:59:17 +0330

    blog post page got responsive.

--- js/blogPost.js
+++ js/blogPost.js
@@ -3,7 +3,14 @@ const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
 
 const card = document.querySelector('.recommend-card');
-const cardWidth = card.offsetWidth + 30;
+
+let currentIndex = 0;
+const totalCards = document.querySelectorAll('.recommend-card').length;
+const visibleCards = window.innerWidth <= 1000 ? 1 : 4;
+
+function getCardWidth() {
+    return card.offsetWidth + 30;
+}
 
 nextBtn.addEventListener('click', () => {
     carousel.style.transform = `translateX(-${getNextOffset(1)}px)`;
@@ -13,14 +20,15 @@ prevBtn.addEventListener('click', () => {
     carousel.style.transform = `translateX(-${getNextOffset(-1)}px)`;
 });
 
-let currentIndex = 0;
-const totalCards = document.querySelectorAll('.recommend-card').length;
-const visibleCards = 4;
-
 function getNextOffset(direction) {
     currentIndex += direction;
     if (currentIndex < 0) currentIndex = 0;
     if (currentIndex > totalCards - visibleCards)
         currentIndex = totalCards - visibleCards;
-    return currentIndex * cardWidth;
+    return currentIndex * getCardWidth();
 }
+
+window.addEventListener('resize', () => {
+    currentIndex = 0;
+    carousel.style.transform = `translateX(0)`;
+});
