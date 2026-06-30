ba2f1a505c35b02c5e847ba44d57ee7a9ae39c24
Negarfrd <negar.fereidouni96@gmail.com>
2026-06-30 14:12:02 +0330

    categories APIs connected.

--- js/home.js
+++ js/home.js
@@ -13,3 +13,41 @@ recipeCards.forEach(card => {
     });
 
 });
+
+const catList = document.getElementById("categoriesList");
+
+async function loadCategories() {
+    try {
+        catList.innerHTML = "<div class=\"loader\"></div>";
+
+        const response = await fetch(
+            "https://foodieland-oq9b.onrender.com/api/categories/"
+        );
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch categories");
+        }
+
+        const categories = await response.json();
+
+        catList.innerHTML = "";
+
+        categories.forEach(category => {
+            const card = document.createElement("div");
+            card.classList.add("cat-card", category.name.toLowerCase());
+
+            card.innerHTML = `
+                <img src="${category.image}" alt="${category.name}">
+                <h3>${category.name}</h3>
+            `;
+
+            catList.appendChild(card);
+        });
+
+    } catch (error) {
+        console.error(error);
+        catList.innerHTML = "<p>Failed to load categories</p>";
+    }
+}
+
+loadCategories();
